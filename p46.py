1cabc16d6a578d4f07f5736f17ca98e84afcd7a7
Raju <rnmodi27@gmail.com>
2024-10-16 23:58:18 +0530

    Program 46,47,48 Done

---
+++ p46.py
@@ -0,0 +1,6 @@
+# Write a Python Program to Generate a Series like 100 99 98 97 …90
+
+x=100
+while(x>=90):
+    print(x)
+    x-=1
---
+++ p47.py
@@ -0,0 +1,8 @@
+# Write a Python Program to Generate a Series like 1 3 5 7 19
+
+
+
+for i in range(1,20):
+    if(i%2!=0):
+        print(i)
+
