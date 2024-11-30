58ff908335ac55256cf564671ab383984d957be2
Raju <rnmodi27@gmail.com>
2024-11-30 17:21:23 +0530

    Progrm 69 few patterns left

---
+++ p69.py
@@ -0,0 +1,179 @@
+# (a)
+# 12345
+# 12345
+# 12345
+# 12345
+# 12345
+
+
+# for i in range(1,6):
+#     for j in range(1, 6):
+#         print(j,end="")
+#     print("\n")
+
+
+
+ # 11111
+ # 22222
+ # 33333
+ # 44444
+ # 55555
+#
+# for i in range(1,6):
+#     for j in range(1, 6):
+#         print(i,end="")
+#     print("\n")
+
+
+
+
+# 1
+#  12
+#  123
+#  1234
+#  12345
+
+
+# for i in range(1,6):
+#     for j in range(1, i+1):
+#         print(j,end="")
+#     print("\n")
+
+
+
+
+# 1
+#  22
+#  333
+#  4444
+#  55555
+
+# for i in range(1,6):
+#     for j in range(0, i+1):
+#         print(i,end="")
+#     print("\n")
+
+
+# 55555
+#  4444
+#  333
+#  22
+#  1
+
+# for i in range(5,0,-1):
+#     for j in range(0, i):
+#         print(i,end="")
+#     print("\n")
+
+
+# 54321
+# 5432
+# 543
+# 54
+# 5
+
+# for i in range(0,5):
+#     for j in range(5, i,-1):
+#         print(j,end="")
+#     print("\n")
+
+
+# 1
+# 21
+# 321
+# 4321
+# 54321
+# for i in range(1, 6):
+#     for j in range(i, 0, -1):
+#         print(j, end="")
+#     print()
+
+# 5
+#  45
+#  345
+#  2345
+#  12345
+# for i in range(5, 0, -1):
+#     for j in range(i, 6):
+#         print(j, end="")
+#     print()
+
+
+
+
+
+ # 12345
+ # 2345
+ # 345
+ # 45
+ # 5
+# for i in range(1, 6):
+#     for j in range(i, 6):
+#         print(j, end="")
+#     print()
+
+
+# 54321
+# 4321
+# 321
+# 21
+# 1
+# for i in range(5, 0,-1):
+#     for j in range(i, 0,-1):
+#         print(j, end="")
+#     print()
+
+
+# 5
+# 54
+# 543
+# 5432
+# 54321
+# for i in range(5, 0,-1):
+#     for j in range(5, i-1,-1):
+#         print(j, end="")
+#     print()
+
+# 246810
+#  46810
+#   6810
+#    810
+#     10
+# number = "246810"
+#
+# for i in range(len(number)):
+#     print(" " * i + number[i:])
+
+
+# (m) DONT KNOW
+# 1
+# 23
+# 456
+# 78910
+# 1112131415
+
+
+
+# 1
+# 01
+# 010
+# 1010
+# 10101
+# for i in range(1, 6):
+#     for j in range(i):
+#         print(j % 2, end="")
+#     print()
+
+
+# 1
+# 10
+# 101
+# 1010
+# 10101
+# # (o) Pattern
+# for i in range(1, 6):
+#     for j in range(i):
+#         print(j % 2, end="")
+#     print()
+
+
