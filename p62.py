8f77a2dddc2dd92ad4613ec87129c95fa818eacc
Raju <rnmodi27@gmail.com>
2024-11-30 15:49:41 +0530

    Program 62 done

---
+++ p62.py
@@ -0,0 +1,7 @@
+# Write a program, which will read an integer value for a base, then read a positive integer raised to that
+#  base and print its value.
+
+base=int(input("enter the base value"))
+raze=int(input("enter the raised value"))
+
+print(base**raze)
